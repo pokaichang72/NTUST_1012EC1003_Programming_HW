0b6994d347e38fb18b77b73e5d568dc04b7480e1
pokaichang72 <pokaichang72@gmail.com>
2013-04-11 06:17:34 +0000

    update hw3

--- hw3/3.c
+++ hw3/3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 
+int gcd(int x, int y)
+{
+	int remainder=1;
+
+	printf("Computing the gcd:\n");
+
+	while ( x || y ) {
+		printf("%d/%d = %d ... %d", x, y, x/y, x%y);
+	}
+}
+
+
 int main()
 {
+	int x, y, remainder;
+
+	whil
 }
