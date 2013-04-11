613dbce473e8b1199475db69d2c278801523c2db
pokaichang72 <pokaichang72@gmail.com>
2013-04-11 22:04:15 +0800

    complete hw3

--- hw3/4.c
+++ hw3/4.c
@@ -4,17 +4,44 @@
 
 int scan_data(char *o, float *v)
 {
-	while (1) {
-		do {
-			scanf("%c", o);  //get the operator
-		} while ( *o == 10 || *o == 13 );  //ignore the Line feed and Carriage return
+	do {
+		scanf("%c", o);  //get the operator
+	} while ( *o == 10 || *o == 13 || *o == 32 );  //ignore the Line feeds, Carriage returns and Spaces which might be scanned as a character and occur errors
 
-		if (*o == 'q') return 0;  //terminate if input 'q' detected
-		if ( !scanf("%f", v) || !( *o == '+' || *o == '-' || *o == '*' || *o == '/' || *o == '^' ) ) {  // get the value and check input
-			printf("Syntax error:\"%c %f\". Please try again.\n", *o, *v);
-			continue;
-		}
-		return 1;
+	if (*o == 'q') return 0;  //terminate if input 'q' detected
+
+	scanf("%f", v);  // get the operand
+	return 1;
+}
+
+
+void do_next_op(char o, float v, float *a)
+{
+	switch (o) {  //do the calculation
+
+		case '+':
+			*a += v;
+			break;
+
+		case '-':
+			*a -= v;
+			break;
+
+		case '*':
+			*a *= v;
+			break;
+
+		case '/':
+			*a /= v;
+			break;
+
+		case '^':
+			*a = pow(*a, v);
+			break;
+
+		default:
+			printf("Unknown operator: %c\n", o);
+			break;
 	}
 }
 
@@ -22,10 +49,14 @@ int scan_data(char *o, float *v)
 int main()
 {
 	char operator;
-	float input_value;
+	float accumulator=0, operand;
 
-	while ( scan_data(&operator, &input_value) ) {
-		printf("%d and %f\n", operator, input_value);
+	while ( scan_data(&operator, &operand) ) {  //get data
+		do_next_op(operator, operand, &accumulator);  //perform the operation
+		printf("result so far is %.1f\n", accumulator);  //print status
 	}
 
+	printf("final result is %.1f\n", accumulator);
+
+	return 0;
 }
