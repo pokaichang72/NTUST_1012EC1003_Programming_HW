979c284901543976324b704d4f3d165db6c9a83b
pokaichang72 <pokaichang72@gmail.com>
2013-04-14 11:12:32 +0000

    update

--- hw3/4.c
+++ hw3/4.c
@@ -6,32 +6,32 @@
 
 int main()
 {
-	int ans, in, c, correct;
-	char yn;
+	int ans, in, correct, c;  //the [answer], [inputtded number], [guessed or not] and the [garbage-can]
+	char yn;  //yes or no
 	srand(time(NULL));
 
 	/* Let's start the game! */
 	while (1) {
-		/* init */
+		/* Initialize variables */
 		yn = '0';
 		correct = 0;
 		ans = (rand()%RANGE)+1;
 
-		printf("I have a number between 1 and 1000.\n");
+		printf("I have a number between 1 and %d.\n", RANGE);
 		printf("Can you guess my number?\n");
 		printf("Please enter your first guess.\n");
 
-		/* guess until you get it right */
+		/* Guess until you get it right */
 		while (!correct) {
 			while ( !scanf("%d", &in) ) {
 				printf("Error: Please enter a integer.\n");
 				while ( (c = getchar()) != '\n' && c != EOF);  //remember to clear the input buffer!
 			}
 
-			if (in > ans) {  //too high
+			if (in > ans) {  //too high!
 				printf("Too high. Try again.\n");
 
-			} else if (in < ans) {  //too low
+			} else if (in < ans) {  //too low!
 				printf("Too low. Try again.\n");
 
 			} else {  //bingo!
@@ -42,9 +42,9 @@ int main()
 				/* ask the user to play again */
 				while (1) {
 					scanf("%c", &yn);  //get input
-					while((c = getchar()) != '\n' && c != EOF); //clear the input buffer
+					while((c = getchar()) != '\n' && c != EOF);  //clear the input buffer
 					if (yn == 'n') {
-						printf("bye\n");
+						printf("bye!\n");
 						return 0;
 					} else if (yn == 'y') {
 						printf("\n");
